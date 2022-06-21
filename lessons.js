cf5743a88ded8b3112e5c31ae50d67b9a2965cf3
creakopps <77118099+creakopps@users.noreply.github.com>
2022-06-21 06:56:57 +0100

    Add files via upload

---
+++ lessons.js
@@ -0,0 +1,112 @@
+
+   let lessons = [
+        {
+            "id": 1,
+            "Subject": "Math",
+            "Location": "Hackney",
+            "price": 120,
+            "Space": 5,
+            "image": "math icon.png"
+        },
+        {
+            "id": 2,
+            "Subject": "English",
+            "Location": "London",
+            "price": 130,
+            "Space": 4,
+            "image": "English icon.png"
+        },
+        {
+            "id": 3,
+            "Subject": "Science",
+            "Location": "Tottenham",
+            "price": 150,
+            "Space": 15,
+            "image": "science icon.png"
+        },
+        {
+            "id": 4,
+            "Subject": "History",
+            "Location": "York",
+            "price": 60,
+            "Space": 5,
+            "image": "history icon.png"
+        },
+        {
+            "id": 5,
+            "Subject": "Music",
+            "Location": "Birmingham",
+            "price": 50,
+            "Space": 7,
+            "image": "Music icon.png"
+        },
+        {
+            "id": 6,
+            "Subject": "Math",
+            "Location": "Leicester",
+            "price": 170,
+            "Space": 6,
+            "image": "math icon.png"
+        },
+        {
+            "id": 7,
+            "Subject": "English",
+            "Location": "Greenwich",
+            "price": 140,
+            "Space": 12,
+            "image": "English icon.png"
+        },
+        {
+            "id": 8,
+            "Subject": "Science",
+            "Location": "Edinburgh",
+            "price": 80,
+            "Space": 15,
+            "image": "science icon.png"
+        },
+        {
+            "id": 9,
+            "Subject": "History",
+            "Location": "Chelsea",
+            "price": 110,
+            "Space": 10,
+            "image": "history icon.png"
+        },
+        {
+            "id": 10,
+            "Subject": "Music",
+            "Location": "Bristol",
+            "price": 120,
+            "Space": 5,
+            "image": "Music icon.png"
+        },
+        { "id": 11,
+          "Subject": "ICT",
+          "Location": "Manchester",
+          "price": 50,
+          "Space": 9,
+          "image": "Ict icon.png"
+        },
+        { "id": 12,
+          "Subject": "Art",
+          "Location": "Sheffield",
+          "price": 80,
+          "Space": 6,
+          "image": "Art icon.png"
+        },
+        { "id": 13,
+          "Subject": "Sports",
+          "Location": "Liverpool",
+          "price": 90,
+          "Space": 8,
+          "image": "sports icon.png"
+        },
+        { "id": 14,
+          "Subject": "Politics",
+          "Location": "Cambridge",
+          "price": 100,
+          "Space": 10,
+          "image": "Politics icon.png"
+        }
+    ]
+        
